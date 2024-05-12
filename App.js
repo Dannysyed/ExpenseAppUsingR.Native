419939cab141096fb57576d0a83928f52dbc3335
Dannysyed <dannysyed20@gmail.com>
2024-05-12 03:26:31 -0400

    building navigation

--- App.js
+++ App.js
@@ -1,65 +1,80 @@
-import React from 'react';
-import { NavigationContainer } from '@react-navigation/native';
-import { createStackNavigator } from '@react-navigation/stack';
-import { createDrawerNavigator } from '@react-navigation/drawer';
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-import { Light_color, Secondary_color } from './components/Colors';
+import React from 'react'
+import { NavigationContainer } from '@react-navigation/native'
+import { createStackNavigator } from '@react-navigation/stack'
+import { createDrawerNavigator } from '@react-navigation/drawer'
+import { StatusBar } from 'expo-status-bar'
+import { StyleSheet, Text, View } from 'react-native'
+import { Light_color, Secondary_color } from './components/Colors'
 import { Ionicons } from '@expo/vector-icons'
 
-import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
-import ManageExpenses from './screens/ManageExpenses';
-import AllExpenses from './screens/AllExpenses';
-import RecentExpense from './screens/RecentExpense';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs'
+import ManageExpenses from './screens/ManageExpenses'
+import AllExpenses from './screens/AllExpenses'
+import RecentExpense from './screens/RecentExpense'
+import { GlobalStyles } from './constants/styles'
 
-const BottomTabs = createBottomTabNavigator();
+const BottomTabs = createBottomTabNavigator()
 
-const Stack = createStackNavigator();
+const Stack = createStackNavigator()
 
 function DrawerScreen() {
   return (
-    <BottomTabs.Navigator initialRouteName="Games"
-
-      tabBarOptions={{
-        activeTintColor: Secondary_color,
-        inactiveTintColor: 'gray',
+    <BottomTabs.Navigator
+      initialRouteName='Games'
+      screenOptions={{
+        headerStyle: {
+          backgroundColor: GlobalStyles.colors.primary500,
+        },
+        headerTintColor: 'white',
+        tabBarStyle: {
+          backgroundColor: GlobalStyles.colors.primary500,
+        },
+        tabBarActiveTintColor: GlobalStyles.colors.accent500,
       }}
-      screenOptions={{ headerStyle: { backgroundColor: 'red' } }}
     >
-
-      <BottomTabs.Screen name="Recent" component={RecentExpense} options={{
-        tabBarIcon: ({ color, size, focused }) => (
-          <Ionicons name="settings" color={focused ? Secondary_color : 'gray'} size={size} />
-        ),
-      }} />
-      <BottomTabs.Screen name="AllExpense" component={AllExpenses}
+      <BottomTabs.Screen
+        name='Recent'
+        component={RecentExpense}
         options={{
-          tabBarIcon: ({ color, size }) => (
-            <Ionicons name="home" color={color} size={size} />
+          title: 'Recent Expenses',
+          tabBarLabel: 'Recent',
+          tabBarIcon: ({ color, size, focused }) => (
+            <Ionicons name='hourglass' color={color} size={size} />
           ),
         }}
       />
-    </BottomTabs.Navigator >
-  );
+      <BottomTabs.Screen
+        name='AllExpense'
+        component={AllExpenses}
+        options={{
+          title: 'All Expense',
+          tabBarLabel: 'All Expense',
+          tabBarIcon: ({ color, size }) => (
+            <Ionicons name='calendar' color={color} size={size} />
+          ),
+        }}
+      />
+    </BottomTabs.Navigator>
+  )
 }
 
 function App() {
   return (
-
     <>
-      <StatusBar style='auto'>
-        <NavigationContainer>
-          <Stack.Navigator>
-            <Stack.Screen name="StartScreen" component={DrawerScreen} options={{ headerShown: false }} />
-            <Stack.Screen name="ManageExpense" component={ManageExpenses} />
-          </Stack.Navigator>
-        </NavigationContainer>
-      </StatusBar>
+      <StatusBar style='light' />
+      <NavigationContainer>
+        <Stack.Navigator>
+          <Stack.Screen
+            name='StartScreen'
+            component={DrawerScreen}
+            options={{ headerShown: false }}
+          />
+          <Stack.Screen name='ManageExpense' component={ManageExpenses} />
+        </Stack.Navigator>
+      </NavigationContainer>
     </>
-  );
+  )
 }
-
-
 
 const styles = StyleSheet.create({
   container: {
@@ -67,6 +82,6 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
-});
+})
 
-export default App;
+export default App
